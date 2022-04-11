9a867730b85764f5f8c875ef208df9f16210ffd1
urajrishuu <urajrishuu@gmail.com>
2022-04-11 21:53:05 +0530

    https://github.com/uraajrishu/uranium.git

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
-
+// const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://urajrishu:aUHDB96UyJaq9SB@cluster0.1wief.mongodb.net/urajrishu02",
+{
+    useNewUrlParser : true 
+})
+.then(  () => console.log("MongoDB is Connected"))
+.catch(err => console.log(err))
+
 
 app.use('/', route);
 
