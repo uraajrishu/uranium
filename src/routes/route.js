f2e3688fece687d88ca4b15ef1c898c3130f139c
urajrishuu <urajrishuu@gmail.com>
2022-04-12 07:41:03 +0530

    https://github.com/uraajrishu/uranium.git

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const bookmodel = require('../bookmodel/bookmodel.js');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+// const UserModel= require("../models/userModel.js")
+// const UserController= require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/bookauthur", async function(req, res){
+ let book = req.body
+ let saveBook= await bookmodel.create(book)
+ res.send({msg: saveBook})   
+}) 
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBookList", async function(req, res){
+    let allBooks= await bookmodel.find()
+    res.send({msg: allBooks})
+    
+})
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
 
 module.exports = router;
